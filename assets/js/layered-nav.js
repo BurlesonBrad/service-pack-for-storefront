af31018bede600e836b1681f75686c84d8ee4527
opportus <opportus@gmail.com>
2016-07-06 14:30:23 +0200

    removed js files

--- assets/js/layered-nav.js
+++
@@ -1,5 +0,0 @@
-jQuery( document ).ready( function( $ ) {
-
-	$( '.widget_layered_nav, .widget_price_filter' ).wrapAll( '<div class="spfs_widget_layered_nav_wrapper">' );
-	$( '.widget_layered_nav' ).first().addClass( 'first' );
-} );
